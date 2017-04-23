d806008b0ccf045de34e2e2afa8734153eb1f260
Paul Em <paul3m@gmail.com>
2017-04-23 16:34:59 +0200

    added about page

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import Paper from 'material-ui/Paper';
+import pkg from '../../package.json';
+
+const styles = {
+  container: {
+    padding: 10,
+    lineHeight: '1.4em',
+  },
+};
 
 export default class About extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      version: pkg.version,
+    };
+  }
+
   render() {
     return <div>
-      <h1>{this.constructor.name}</h1>
+      <h1>About</h1>
+      <Paper style={styles.container}>
+        <p>
+          Developer: Paul Em<br/>
+          Contact: info@paulem.eu<br/>
+          Version: {this.state.version}
+        </p>
+        <h2>Rechtliche Information</h2>
+        <p>
+          Dieses Programm ist kein offizielles Program des Österreichischen Rundfunks.
+          Alle Services zu dieser Radio Station können
+          unter der offiziellen Website abgerufen werden.
+        </p>
+      </Paper>
     </div>;
   }
 }
