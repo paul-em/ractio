7fdb46ed45629aa2db7c2084de3412cc192e4d93
Paul Em <paul3m@gmail.com>
2017-04-23 14:48:55 +0200

    added stations route with controlled inputs

--- src/routes/Stations.js
+++ src/routes/Stations.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { RadioButton, RadioButtonGroup } from 'material-ui/RadioButton';
+import Paper from 'material-ui/Paper';
+import stations from '../stations';
+
+const styles = {
+  container: {
+    padding: 10,
+  },
+  radioButton: {
+    padding: 10,
+  },
+};
 
 export default class Stations extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: 'fm4',
+    };
+  }
+
+  setStation(shortName) {
+    this.setState({
+      selected: shortName,
+    });
+  }
+
   render() {
     return <div>
       <h1>{this.constructor.name}</h1>
+      <Paper style={styles.container}>
+        <RadioButtonGroup name="stations" valueSelected={this.state.selected}
+                          onChange={(e, value) => this.setStation(value)}>
+          {stations.map(station => <RadioButton value={station.shortName}
+                                                label={station.name}
+                                                key={station.shortName}
+                                                style={styles.radioButton}/>)}
+        </RadioButtonGroup>
+      </Paper>
     </div>;
   }
 }
